12279084cd7b169f68ddac86cf2bbc95f5b120d4
holhen <holhen@gmail.com>
2019-09-09 16:09:16 +0200

    Updates

---
+++ precache-manifest.409d7c7b31ab7af2ac32b26878a37496.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fb9720a8a25f6a41825ffa010f1449a",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "c8cd69d6a965dc1eef09",
+    "url": "/Calculator/static/css/main.a5e53274.chunk.css"
+  },
+  {
+    "revision": "914d1d7948a49a1a4f7b",
+    "url": "/Calculator/static/js/2.74b384a5.chunk.js"
+  },
+  {
+    "revision": "c8cd69d6a965dc1eef09",
+    "url": "/Calculator/static/js/main.a7a25102.chunk.js"
+  },
+  {
+    "revision": "cf49d768c4d3a5b9fc25",
+    "url": "/Calculator/static/js/runtime~main.8078b280.js"
+  }
+]);
